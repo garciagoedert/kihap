7b372127b14f5afbdf563f77cd73870989eedcae
Paulo <garciapaulo1995@gmail.com>
2025-09-05 13:34:16 -0300

    Converte o submódulo intranet em um diretório regular

---
+++ intranet/auth.js
@@ -0,0 +1,33 @@
+const users = [
+    { name: 'Paulo', email: 'comercial@kihap.com.br', password: 'Pg1308@!', isAdmin: true, profilePicture: 'default-profile.svg' }
+];
+
+function findUser(email, password) {
+    return users.find(user => user.email === email && user.password === password);
+}
+
+window.getAllUsers = function() {
+    return users;
+}
+
+function addUser(user) {
+    users.push(user);
+}
+
+function updateUser(email, updatedData) {
+    const userIndex = users.findIndex(user => user.email === email);
+    if (userIndex !== -1) {
+        users[userIndex] = { ...users[userIndex], ...updatedData };
+        return true;
+    }
+    return false;
+}
+
+function deleteUser(email) {
+    const userIndex = users.findIndex(user => user.email === email);
+    if (userIndex !== -1) {
+        users.splice(userIndex, 1);
+        return true;
+    }
+    return false;
+}
---
+++ intranet/firebase-config.js
@@ -0,0 +1,24 @@
+// Importa as funções necessárias do SDK do Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
+
+// Configuração do seu aplicativo da web do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD1WKwAsAicGdRz9cRA2Nvv3LEZve1vZe0",
+  authDomain: "intranet-kihap.firebaseapp.com",
+  projectId: "intranet-kihap",
+  storageBucket: "intranet-kihap.firebasestorage.app",
+  messagingSenderId: "1055939458006",
+  appId: "1:1055939458006:web:1d67459a0bc0da60cf2a77",
+  measurementId: "G-5LP0W3QSVZ"
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Exporta a instância do Firestore, o app e o appId para serem usados em outras partes do aplicativo
+export { app };
+export const db = getFirestore(app);
+export const appId = firebaseConfig.appId || 'default-kihap-app';
